dbdec07109da1cc66c00e0168103b970b2f5e6de
Daniel Nussenbaum <daninus14@gmail.com>
2023-11-14 17:57:27 +0200

    adding some sample commands for conversion

--- new-spec/buildhtml.sh
+++ new-spec/buildhtml.sh
@@ -3,3 +3,16 @@ make4ht -d ./output/html/ ./ "mathml" | less
 make4ht -d ./output/html/ chap-1.tex "mathml" | less
 make4ht -d ./output/html/ ./spec-source/chap-1.dvi.Z "mathml"
 make4ht -d ../output/html/ chap-1.dvi.Z "mathml"
+
+
+# makehtml.sh
+# make4ht file.tex "mathml"
+# make4ht -d ./output/html/
+make4ht -d ./output/html/ chap-1.tex "mathml" | less
+
+
+
+make4ht -f html5+detect_engine chap-1.tex
+make4ht -f html5+detect_engine chap-6.tex
+make4ht -f html5+detect_engine chap-6.tex
+
---
+++ new-spec/spec-source/converttohtml.sh
@@ -0,0 +1,2 @@
+make4ht -f html5+detect_engine chap-1.tex
+make4ht -f html5+detect_engine chap-6.tex
